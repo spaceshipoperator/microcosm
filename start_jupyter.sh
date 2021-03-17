846f23c1e4e7c04ad35fe8728e8d5652671078f1
Brian Muckian <bpmuckian@gmail.com>
2021-03-17 12:04:32 -0700

    added useful notebook extensions

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,1 +1,3 @@
+jupyter contrib nbextension install --system
+jupyter nbextensions_configurator enable --system
 jupyter notebook --certfile=./cert.pem --keyfile=key.pem --port 8443 --ip 0.0.0.0 --no-browser --no-mathjax --allow-root --notebook-dir=/usr/src/app/data
